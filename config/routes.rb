8eb300719b494e11ee54384827192afaeb90eee4
V.Protsan <valeriyapro@gmail.com>
2018-03-08 12:43:46 -0500

    pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  #                      #welcome controller, HOME action(method)
+  root 'welcome#home'
+
+  get 'about' => 'welcome#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
